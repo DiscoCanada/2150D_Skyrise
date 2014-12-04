e97fefbe06b5425fbf7b5d7bfb9f93ad0c23e277
HFreni <hfreni@chadwickschool.org>
2014-12-03 22:10:16 -0800

    Push V22

---
+++ Competition V22/auton.h
@@ -0,0 +1,90 @@
+void resetAll()
+{
+	startTask(handleEncoders);
+	startTask(trackXYT);
+
+}
+
+void autonomous0()
+{
+	resetAll();
+}
+
+void autonomous1()
+{
+
+}
+
+void autonomous2()
+{
+
+}
+void autonomous3()
+{
+
+}
+
+void autonomous4()
+{
+
+}
+void autonomous5()
+{
+
+}
+void autonomous6()
+{
+
+}
+void autonomous7()
+{
+
+}
+
+void autonomous8()
+{
+
+}
+
+void autonomous9()
+{
+
+}
+
+void autonomous10()
+{
+
+}
+
+void autonomous11()
+{
+
+}
+
+void autonomous12()
+{
+
+}
+
+void autonomous13()
+{
+
+}
+
+void autonomous14()
+{
+
+}
+
+void autonomous15()
+{
+
+}
+void testing()
+{
+
+}
+void programming()
+{
+
+}
